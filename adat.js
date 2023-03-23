c3d13587723c11b3019a6b4a2b9a834ba9c93602
Szöllősi <ballint3@gmail.com>
2023-03-23 14:59:09 +0100

    qw

---
+++ adat.js
@@ -0,0 +1,12 @@
+export const SZAMLISTA = [23, 5, 55, 11, 121];
+
+
+export const SZOVEGLISTA = ["béka", "alma", "zeller", "Körte"];
+
+export const OBJEKTUMLISTA = [
+    {név: "Dézi", fajta: "keverék", kor: 2},
+    {név: "Bodri", fajta: "mudi", kor: 12},
+    {név: "Loki", fajta: "tacskó", kor: 5},
+    {név: "Zuri", fajta: "foxterrier", kor: 4},
+    {név: "Happy", fajta: "németjuhász", kor: 42},
+]
---
+++ script.js
@@ -0,0 +1,14 @@
+import {OBJEKTUMLISTA} from "/.adat.js";
+
+$(function (){
+    const tablazat=$("article")
+    tablazat.append(osszeallit)
+})
+$(function osszeallit(){
+    let kiir="";
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+
+})
